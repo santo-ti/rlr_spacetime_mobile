e2e0cd03fdc07643c5db633d1f47dbc7a5296c43
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2024-08-25 21:31:50 -0300

    feat: Add expo-google-fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import { BaiJamjuree_700Bold, useFonts } from '@expo-google-fonts/bai-jamjuree'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-zinc-900">
-      <Text className="text-5xl font-bold text-zinc-50">RLRs</Text>
+      <Text className="font-title text-5xl text-zinc-50">RLRs</Text>
       <StatusBar style="light" translucent />
     </View>
   )
