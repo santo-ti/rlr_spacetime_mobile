436591b8960374feb80f3c6a59a588f5787f6b4c
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2024-09-05 16:00:02 -0300

    feat: Add home page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import { BaiJamjuree_700Bold, useFonts } from '@expo-google-fonts/bai-jamjuree'
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { styled } from 'nativewind'
 import bgBlur from './src/assets/img/bg-blur.png'
+import NLWLogo from './src/assets/img/nlw-spacetime-logo.svg'
 import Stripes from './src/assets/img/stripes.svg'
 
 const StyledStripes = styled(Stripes)
@@ -23,12 +24,37 @@ export default function App() {
   return (
     <ImageBackground
       source={bgBlur}
-      className="relative flex-1 items-center bg-zinc-900"
+      className="relative flex-1 items-center bg-zinc-900 px-9 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
 
-      <Text className="font-title text-5xl text-zinc-50">RLRs</Text>
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            CADASTRAR LEMBRANçA
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 por Anderson Santo
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
